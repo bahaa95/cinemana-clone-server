06a66a24cb8c63d633bf0429317bf991a85c9af3
bahaa95 <probahaa3@gmail.com>
2023-04-12 22:07:22 +0300

    refactor: refactor cors middleware

--- src/middleware/cors/index.ts
+++ src/middleware/cors/index.ts
@@ -1,12 +1,11 @@
 import corsPackage from 'cors';
-import { isDevelopment } from '@/utils/isDevelopment';
 import { CLIENT_ORIGIN, DASHBORAD_ORIGIN } from '@/config/index';
 
 // eslint-disable-next-line
 export function cors() {
   return corsPackage({
     credentials: true,
-    origin: isDevelopment() ? '*' : [CLIENT_ORIGIN, DASHBORAD_ORIGIN],
+    origin: [CLIENT_ORIGIN, DASHBORAD_ORIGIN],
     optionsSuccessStatus: 200,
   });
 }
